f3ba86b52fd8936dc4e6268e51282bf1cdfd8a1e
Harshv1311 <harshv1311@gmail.com>
2017-06-06 08:56:51 +0530

    Add files via upload

--- mail_handler.php
+++ mail_handler.php
@@ -8,14 +8,15 @@ $msg=$_POST['msg'];
 $to='harshv1311@gmail.com';
 $subject='Project Description';
 $message="Name: ".$name."\n"."Phone: ".$phone."\n"."Wrote the following: "."\n\n".$msg;
-$headers="From: ".$email;
-$sent=mail($to, $subject, $message,$headers)
+$header="From: ".$email;
+$header="content-type: text/html\r\n";
+$sent=mail($to, $subject, $message,$header);
 if($sent){
-echo"<h1>Sent successfully! Thank you"." ".$name.", I will contact you soon!<h1>";
+echo "<h1>Sent successfully! Thank you"." ".$name.", I will contact you soon!</h1>";
 }
 else
 {
-echo"something went wrong!";
+echo "something went wrong!";
 }
 ?>
 </body>
